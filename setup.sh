2607c651853c593f779d9de4f44ea5717d7d3e5f
Patricio Yael Reller <patricioyr@gmail.com>
2024-03-17 13:55:06 +0000

    Experimenting dockerfile

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-set -e  # Exit immediately if a command exits with a non-zero status.
+# Stop on first error
+set -e
 
-# Update and install dependencies
+# Install system dependencies
 sudo apt-get update
 sudo apt-get install -y libegl1-mesa gfortran libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xkb1 libxkbcommon-x11-0 libxcb-cursor0
 
-# Navigate to the repository's root directory
+# Navigate to the correct directory
 cd /workspaces/exostriker
 
-# Upgrade pip and install necessary Python packages
+# Upgrade pip and install dependencies
 pip install --upgrade pip
 pip install build
 
-# Build and install the Python package
+# Build and install the package
 python3 -m build
 pip install .
